13d0eef8e76e393a19bb15a81c00d783aaaa528f
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-11-01 00:49:16 +0900

    ♻️ refactor: 코드의 변수명을 변경합니다.

--- week1-baseball/src/App.js
+++ week1-baseball/src/App.js
@@ -1,76 +1,56 @@
-import {Console, MissionUtils} from "@woowacourse/mission-utils";
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
     async play() {
         Console.print("숫자 야구게임을 시작합니다.")
-        // 2. 정답일 때 까지 반복시키기
         let isOut = false
 
-        // 1. 중복되지 않는 랜덤한 숫자 (3자릿수) 만들기
         function randomNumberMaker() {
-            const NUMBER_ARRAY = [];
-            while (NUMBER_ARRAY.length < 3) {
-                const NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
-                if (!NUMBER_ARRAY.includes(NUMBER)) {
-                    NUMBER_ARRAY.push(NUMBER);
+            const randomNumberArray = [];
+            while (randomNumberArray.length < 3) {
+                const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+                if (!randomNumberArray.includes(randomNumber)) {
+                    randomNumberArray.push(randomNumber);
                 }
             }
-            return NUMBER_ARRAY
+            return randomNumberArray.join('');
         }
 
-        // 2.1 입력한 숫자의 유효성 검사
-        function checkNumber(input) {
-            // 2.1.1 숫자가 입력이 되었는가?
-            if (isNaN(input)) throw new Error("[ERROR]");
-            // 2.1.2 3자리수가 입력이 되었는가?
-            if (String(input).length !== 3) throw new Error("[ERROR]");
-            // 2.1.3 모두 다른 숫자가 입력이 되었는가?
-            const STRING_INPUT = String(input)
-            if (STRING_INPUT[0] === STRING_INPUT[1] || STRING_INPUT[1] === STRING_INPUT[2] || STRING_INPUT[2] === STRING_INPUT[0]) throw new Error("[ERROR]");
+        function validation(number) {
+            if (isNaN(Number(number))) throw new Error("[ERROR] 숫자를 입력하지 않았습니다.");
+            if (number.length !== 3) throw new Error("[ERROR] 숫자의 길이가 3이상이지 않습니다.");
+            if (number[0] === number[1] || number[1] === number[2] || number[2] === number[0]) throw new Error("[ERROR] 동일한 숫자가 입력이 되었습니다.");
         }
 
-        // 2.2 입력한 숫자와의 랜던한 값의 일치 검사
+        function baseGame(randomNumber, pickNumber) {
+            let strikeCount = 0;
+            let ballCount = 0;
 
-        function baseGame(randomNumberArray, inputNumber) {
-            const RANDOM_STRING = randomNumberArray.join('')
-            const INPUT_STRING = String(inputNumber)
-            let strike_count = 0;
-            let ball_count = 0;
-
-            // 2.2.1 모든 숫자가 일치하는지 확인하기
-
-            if (RANDOM_STRING === INPUT_STRING) {
+            if (randomNumber === pickNumber) {
                 isOut = true;
                 return "3스트라이크"
             }
 
-            const INPUT_NUMBER_ARRAY = INPUT_STRING.split('');
 
-            for (let i in INPUT_NUMBER_ARRAY) {
-                if (RANDOM_STRING.includes(INPUT_NUMBER_ARRAY[i])) {
-
-                    // 2.2.2 자리와 값이 일치하는 수 골라내기 = strike
-                    // 2.2.3 자리는 다르지만 값이 일치하는 수 골라내기 = ball
-                    RANDOM_STRING[i] === (INPUT_NUMBER_ARRAY[i]) ? strike_count++ : ball_count++;
+            for (let i = 0; i < pickNumber.length; i++) {
+                if (randomNumber.includes(pickNumber[i])) {
+                    randomNumber[i] === (pickNumber[i]) ? strikeCount++ : ballCount++;
                 }
             }
 
-            if (!strike_count && !ball_count) return "낫싱";
+            if (!strikeCount && !ballCount) return "낫싱";
+            if (!strikeCount && ballCount) return `${ballCount}볼`;
+            if (strikeCount && !ballCount) return `${strikeCount}스트라이크`;
 
-            if (!strike_count && ball_count) return `${ball_count}볼`;
-
-            if (strike_count && !ball_count) return `${strike_count}스트라이크`;
-
-            return `${ball_count}볼 ${strike_count}스트라이크`
+            return `${ballCount}볼 ${strikeCount}스트라이크`
         }
 
-        const RANDOM_NUMBER_ARRAY = randomNumberMaker();
+        const randomNumber = randomNumberMaker();
 
         while (!isOut) {
-            const STRING_INPUT = await Console.readLineAsync("숫자를 입력해주세요 : ")
-            const NUMBER_INPUT = Number(STRING_INPUT)
-            checkNumber(NUMBER_INPUT);
-            Console.print(baseGame(RANDOM_NUMBER_ARRAY, STRING_INPUT));
+            const pickNumber = await Console.readLineAsync("숫자를 입력해주세요 : ")
+            validation(pickNumber);
+            Console.print(baseGame(randomNumber, pickNumber));
         }
 
         Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
